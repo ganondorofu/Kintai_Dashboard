e79804394a43e085dc03c133baee866ec143ee4e
ganondorofu <ganondorofu123@gmail.com>
2025-08-21 03:56:25 +0000

    handler.js:191  GET https://club-attendance-nzf1k.firebaseapp.com/__/fi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, signInWithRedirect, GithubAuthProvider, signOut, getRedirectResult } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, doc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
